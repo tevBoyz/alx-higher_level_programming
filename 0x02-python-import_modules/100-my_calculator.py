14552ab47f897c7494004303a8c98a4dc5a1ba9a
Dawit <dawit.ttamiru@gmail.com>
2022-06-30 13:11:04 -0700

    task 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -10,19 +10,19 @@ if __name__ == "__main__":
     if argc != 3:
         print('Usage: ./100-my_calculator.py <a> <operator> <b>')
         exit(1)
-    elif argv[1] not in '+-*/':
+
+    a = int(argv[0])
+    b = int(argv[2])
+    op = argv[1]
+
+    if op == '+':
+        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    elif op == '-':
+        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    elif op == '*':
+        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    elif op == '/':
+        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+    else:
         print('Unknown operator. Available operators: +, -, *, and /')
         exit(1)
-    else:
-        a = int(argv[0])
-        b = int(argv[2])
-        op = argv[1]
-
-        if op == '+':
-            print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-        elif op == '-':
-            print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-        elif op == '*':
-            print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-        else:
-            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
