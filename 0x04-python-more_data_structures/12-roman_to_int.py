49fc0b361cf3d12a0412277a17d7000fffee4d89
Dawit <dawit.ttamiru@gmail.com>
2022-07-06 11:03:06 -0700

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,25 +2,24 @@
 def getInt(c):
     rom = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
     val = [1, 5, 10, 50, 100, 500, 1000]
-    
     return val[rom.index(c)]
 
 
 def roman_to_int(roman_string):
-    if roman_string is None:
+    if not isinstance(roman_string,str):
         return 0
     pre = 0
-    sum = 0
+    summ = 0
     rev = reversed(roman_string)
     for i, c in enumerate(rev):
         cur = getInt(c)
         v = len(roman_string) - 1
         if v - i == v:
-            sum += cur
+            summ += cur
         else:
             if cur < pre:
-                sum -= cur
+                summ -= cur
             else:
-                sum += cur
+                summ += cur
         pre = cur
     return sum
