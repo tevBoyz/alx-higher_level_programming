3ef23494cb4091878597090c14d948a00ddfbbc7
Dawit <dawit.ttamiru@gmail.com>
2022-07-06 11:21:13 -0700

    task 100

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
+    if my_list is None:
+        return 0
     return sum([x[0] * x[1] for x in my_list]) / \
             (sum([x[1] for x in my_list] or 1))
