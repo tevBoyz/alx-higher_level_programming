405e3885c02c6f564ad509ec7d197eb3c4ab95bb
Dawit <dawit.ttamiru@gmail.com>
2022-07-06 12:08:36 -0700

    task 6

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/pyhton3
-def print_sorted_dictionary(a_dictionar):
-    for k in sorted(a_dictionar.keys()):
-        print("{}: {}".format(k, a_dictionar[k]))
+def print_sorted_dictionary(a_dictionary):
+    for k in sorted(a_dictionary.keys()):
+        print('{}: {}'.format(k, a_dictionary[k]))
