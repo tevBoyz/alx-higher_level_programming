26f32cd7de48ba3c09a3caf13fffe067fd0bab25
Dawit <dawit.ttamiru@gmail.com>
2022-07-06 05:47:59 -0700

    task 8

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/pyhton3
 def simple_delete(a_dictionary, key=""):
-    if key is None or key not in a_dictionary:
+    if key is None or a_dictionary is None or key not in a_dictionary:
         return a_dictionary
     else:
         del(a_dictionary[key])
