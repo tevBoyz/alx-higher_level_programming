454040f714a47b1c3c5cfff995bec2e8655ec041
Dawit <dawit.ttamiru@gmail.com>
2022-07-19 13:35:25 -0700

    task 103

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Magic class """
+
+import math
+
+
+class MagicClass:
+    """define a circle"""
+
+    def __init__(self, radius=0):
+        """constructor
+        Args:
+            radius (float or int): radius of circle
+        """
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError("radius must be a number")
+        self.__radius = radius
+
+    def area(self):
+        """get area of a circle"""
+        return (self.__radius ** 2 * math.pi)
+
+    def circumferernce(self):
+        """get circumference of circle"""
+        return (2 * math.pi * self.__radius)
