e83ce0d0c2ab05b7cb1ce93c6bdf573eca403b55
Dawit <dawit.ttamiru@gmail.com>
2022-07-19 06:43:13 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Square Class"""
+
+
+class Square:
+    """Defines Square class"""
+
+    def __init__(self, size):
+        """ constructor,
+
+        Args:
+            size: lenght of sides
+        """
+        self.__size = size
