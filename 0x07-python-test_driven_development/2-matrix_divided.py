6b900b6eb07716952d2069bfe6ea3f66da0eb543
Dawit <dawit.ttamiru@gmail.com>
2022-07-28 06:57:31 -0700

    task 1 and 2

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Defines a matrix division"""
+
+
+def matrix_divided(matrix, div):
+    """divide all values of a matrix
+
+    Args:
+        matrix: the matrix
+        div: divisor
+    Raised:
+        TypeError: if matrix has non-numbers
+        TypeError: if matrix has rows of different sizes
+        TypeError: if div is not an int or float
+        ZeroDivisionError: if div is 0.
+    Returns:
+        A new matrix
+    """
+    if (not isinstance(matrix, list) or matrix ==[] or
+            not all(isinstance(row, list) for  row in matrix) or
+            not all(isinstance(ele, int) or isinstance(ele, float))
+                    for ele in [num for row in matrix for num in row])):
+        raise TypeError("matrix must be a matrix (list of lists) of "
+                "integers/floats")
+
+    if not all(len(row) == len(matrix[0]) for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+
+    if not isinstance(dic, int) and not isinstance(div, float):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
