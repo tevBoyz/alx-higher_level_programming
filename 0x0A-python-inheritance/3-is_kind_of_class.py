6ea366b5b634a1c2d1bf4608435836d5401e4b8b
Dawit <dawit.ttamiru@gmail.com>
2022-08-01 12:43:14 -0700

    task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" class is_same class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Args:
+        obj: object
+        a_classs: class
+    Return: true or false
+    """
+    return isinstance(obj, a_class)
