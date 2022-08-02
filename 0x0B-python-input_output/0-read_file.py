ee486c7b876443169f000523428933a5fb010a12
Dawit <dawit.ttamiru@gmail.com>
2022-08-02 11:48:54 -0700

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ func that reads a text file"""
 
+
 def read_file(filename=""):
     """reads a utf8 file and prints to stdout"""
     with open(filename, "r", encoding="utf-8") as f:
